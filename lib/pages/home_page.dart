15e461f84c11558cca140c24683f99e5b13c3f97
daksh <dakshyt1205@gmail.com>
2024-01-28 18:56:03 +0530

    update: filters and modifiers

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -3,6 +3,22 @@ import 'package:supabase_flutter/supabase_flutter.dart';
 import 'edit_page.dart';
 import 'create_page.dart';
 
+// Filters
+
+// 1. Equal to : supabase.from('users').select().eq('name','The Rock');
+// 2. Not Equal to : supabase.from('users').select().neq('name','Bill Gates');
+// 3. Greater than : supabase.from('users').select().gt('age', 18);
+// 4. Greater than or Equal to : supabase.from('users').select().gte('followers', 10000);
+// 5. Less than : lt() and Less than or Equal to : lte()
+// 6. Column matches a pattern (case sensitive) : supabase.from('users').select().like('name', '%The%');
+// 7. Column matches a pattern (case insensitive) : supabase.from('users').select().ilike('name', '%the%');
+// 8. Column is in the array : supabase.from('users').select().in_('status', ['ONLINE', 'OFFLINE']);
+
+// Modifiers
+
+// 1. Order : supabase.from('users').select().order('id', ascending: false);
+// 2. Limit the query : supabase.from('users').select().limit(10);
+
 class HomePage extends StatelessWidget {
   HomePage({super.key});
   final SupabaseClient supabase = Supabase.instance.client;
@@ -11,7 +27,11 @@ class HomePage extends StatelessWidget {
   // supabase.from('todos').select();
 
   Future<List> readData() async {
-    final result = await supabase.from('todos').select();
+    final result = await supabase
+        .from('todos')
+        .select()
+        .eq('user_id', supabase.auth.currentUser!.id)
+        .order('id', ascending: false);
     return result;
   }
 
